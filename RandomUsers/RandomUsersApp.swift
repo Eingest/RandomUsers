9bb6559eb17aaba759dbe541c05463a4263a4ff8
Eingest <ak0805@live.de>
2022-06-23 17:37:28 +0200

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Andreas Kiesel on 23.06.22.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
